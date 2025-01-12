c64852ff121d27e1b859ee05e0cf4841aba64975
Yash Garg <yash.gargji2004@gmail.com>
2025-01-13 03:20:28 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import dotenv from 'dotenv'
 import connectDB from "../db/index.js";
+import { app } from './app.js';
 
 dotenv.config()
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is ruuning at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection failed",err);
+    
+})
---
+++ utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+// const asyncHnadler = (fn) => async (req,res,next) => {
+//   try {
+//       await fn(req,res,next)
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//         success: false,
+//         message: error.message
+//     })
+//   }
+// }
+
+export default asyncHandler
