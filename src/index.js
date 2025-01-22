3d7f5865f1a0c0776166634361134c52e0ed6eb1
Yash Garg <yash.gargji2004@gmail.com>
2025-01-22 21:23:41 +0530

    check

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv'
 import connectDB from "../db/index.js";
-import { app } from './app.js';
+import {app} from './app.js';
 
-dotenv.config()
-
+ dotenv.config()
+ 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`server is ruuning at port: ${process.env.PORT}`);
+        console.log(`server is running at port: ${process.env.PORT}`);
         
     })
 })
